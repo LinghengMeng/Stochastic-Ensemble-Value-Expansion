52653d29736ffd07c3d7d928a58713ded6aae076
Lingheng <linghengmeng@yahoo.com>
2020-02-02 11:37:34 -0500

    output_root directory with date-time

--- config.py
+++ config.py
@@ -33,9 +33,10 @@ config["resume"] = args.resume
 
 cstr = str(config)
 
-def log_config():
-  # HPS_PATH = util.create_directory("{}_output/".format(formatted_time) + config["env"]["name"] + "/" + config["name"] + "/" + config["log_path"]) + "/hps.json"
-  HPS_PATH = util.create_directory("output/" + config["env"]["name"] + "/" + config["name"] + "/" + config["log_path"]) + "/hps.json"
+def log_config(formatted_time):
+  HPS_PATH = util.create_directory("{}_output/".format(formatted_time) + config["env"]["name"] + "/" + config["name"] + "/" + config["log_path"]) + "/hps.json"
+  config["output_root"] = "{}_output".format(formatted_time)
+
   print("ROOT GPU: " + str(args.root_gpu) + "\n" + str(cstr))
   with open(HPS_PATH, "w") as f:
     f.write("ROOT GPU: " + str(args.root_gpu) + "\n" + str(cstr))
--- master.py
+++ master.py
@@ -32,19 +32,12 @@ MODEL_AUGMENTED = config["model_config"] is not False
 
 formatted_time = time.strftime("%Y-%m-%d-%H-%M")
 
-# if config["resume"]:
-#   ROOT_PATH = "{}_output/".format(config["resume_date"]) + config["env"]["name"] + "/" + config["name"]
-#   log_config(config["resume_date"])
-# else:
-#   ROOT_PATH = util.create_and_wipe_directory("{}_output/".format(formatted_time) + config["env"]["name"] + "/" + config["name"])
-#   log_config(formatted_time)
-
 if config["resume"]:
-  ROOT_PATH = "output/" + config["env"]["name"] + "/" + config["name"]
+  ROOT_PATH = "{}_output/".format(config["resume_date"]) + config["env"]["name"] + "/" + config["name"]
+  log_config(config["resume_date"])
 else:
-  ROOT_PATH = util.create_and_wipe_directory("output/" + config["env"]["name"] + "/" + config["name"])
-
-log_config()
+  ROOT_PATH = util.create_and_wipe_directory("{}_output/".format(formatted_time) + config["env"]["name"] + "/" + config["name"])
+  log_config(formatted_time)
 
 if MODEL_AUGMENTED: import worldmodel_learner
 
